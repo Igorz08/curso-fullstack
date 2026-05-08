917f5eb981bc6fc3f7371da3087197135d094168
Igorz08 <igoroliveiramacedo12.03.2008@gmail.com>
2026-05-08 14:41:00 -0300

    usando o 'for'

--- mine-site/js/script.js
+++ mine-site/js/script.js
@@ -1,23 +1,3 @@
-let valor = 7;
-
-console.log(valor)
-
-valor = valor + 1
-
-console.log("agora vale",valor)
-
-valor++;
-
-console.log("agora vale",valor)
-
-valor += 4
-
-console.log("agora vale",valor)
-
-valor -= 3
-
-console.log("agora vale",valor)
-
-valor--
-
-console.log("agora vale",valor)
+for (let i = 0; i <=5; i++) {
+    console.log("O valor de i vale",i)
+}
