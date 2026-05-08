de129636ee761b3e4f401c84c630246f9d2362f4
Igorz08 <igoroliveiramacedo12.03.2008@gmail.com>
2026-05-08 14:54:01 -0300

    exercicio usando o 'for'

--- mine-site/js/script.js
+++ mine-site/js/script.js
@@ -1,3 +1,5 @@
-for (let i = 0; i <=5; i++) {
-    console.log("O valor de i vale",i)
+for (let x = 1; x <= 10; x++) {
+ //   console.log("Esta é a",x+"ª vez que esta frase é exibida no console")
+    console.log(`Esta é a ${x}ª vez que esta frase é exibida no console`)
+
 }
