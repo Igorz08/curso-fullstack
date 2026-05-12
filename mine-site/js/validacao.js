50cfc622c4f34d8185ea79355b38565623b405db
Igorz08 <igoroliveiramacedo12.03.2008@gmail.com>
2026-05-12 16:19:26 -0300

    Limpando dados depois de enviar

--- mine-site/js/validacao.js
+++ mine-site/js/validacao.js
@@ -5,13 +5,36 @@ formulario.addEventListener("submit", function() {
 
     const nome = document.getElementById("nome");
     const telefone = document.getElementById("Telefone");
-    const sexo = document.getElementById("Sexo")
+    const sexo = document.getElementById("Sexo");
+    const checkbox = document.getElementById("Sim")
 
     console.log(nome.value);
     console.log(telefone.value);
     console.log(sexo.value);
     
+    // validação
+    if (nome.value.trim() === "") {
+        alert("Preencha o nome");
+        return false;
+    }
+
+    if (telefone.value.trim() === "") {
+        alert("Preencha o telefone")
+        return false
+    }
+
+    // Limpar campos
+
+    nome.value = "";
+    telefone.value = "";
+    checkbox.checked = false;
+
+    alert("Formulario enviado!");
 
 });
+
+// .trim: remove os espaços
+// == compara o tipo ou valor
+// === compara o tipo E valor
 
 // addEventListener: escutador de evento
